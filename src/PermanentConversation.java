61c4e3d11fe78a41b25b9a3f412859a682ef821d
William Loughton <u7283599@anu.edu.au>
2022-04-29 23:55:40 +1000

    Delete PermanentConversation.java

--- src/PermanentConversation.java
+++
@@ -1,5 +0,0 @@
-public class PermanentConversation extends Conversation {
-    public PermanentConversation(User user1, Language language1, User user2, Language language2) {
-        super(user1, language1, user2, language2);
-    }
-}
