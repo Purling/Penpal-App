02abd65fe98568a695759dd5903f47a3f089445c
Xingkun Chen <u6710622@anu.edu.au>
2022-05-05 16:19:03 +1000

    Some basic GUI

---
+++ app/src/main/java/com/example/comp2100_assignment/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.comp2100_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button friend;
+    private Button search;
+    private EditText input;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input_text);
+        friend = findViewById(R.id.friend);
+        friend.setOnClickListener(friend_listener);
+        search = findViewById(R.id.search);
+        search.setOnClickListener(search_listener);
+        listView = findViewById(R.id.my_lv);
+        //listView.setOnItemClickListener(list_listener);
+
+
+    }
+
+    public View.OnClickListener search_listener = (view) -> {
+        Intent intent = new Intent();
+        intent.putExtra("search",input.getText().toString());
+        intent.setClass(MainActivity.this, SearchActivity.class);
+        startActivity(intent);
+    };
+    public View.OnClickListener friend_listener = (view) -> {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, FriendActivity.class);
+        startActivity(intent);
+    };
+    //public AdapterView.OnItemClickListener list_listener = (view)
+
+}
