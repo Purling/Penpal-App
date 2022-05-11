a28fdcbea1961b3c93f66ef0e522a346ff49ea70
William <u7283599@anu.edu.au>
2022-05-11 22:25:35 +1000

    User's profile picture is displayed

--- app/src/main/java/com/example/comp2100_assignment/MainActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +27,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
 public class MainActivity extends AppCompatActivity {
     private Button friend;
     private Button search;
@@ -41,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        manager = DatabaseUserManager.getInstance(getBaseContext());
+
+        Intent intent = getIntent();
+        user = (UserPartial) intent.getSerializableExtra("USER");
+
         input = findViewById(R.id.input_text);
         friend = findViewById(R.id.friend);
         friend.setOnClickListener(friend_listener);
@@ -50,18 +63,32 @@ public class MainActivity extends AppCompatActivity {
         match = findViewById(R.id.match);
         match.setOnClickListener(match_listener);
         profilePicture = findViewById(R.id.profile_picture);
-
-        //todo: find and display profile picture if it exists
-
-        manager = DatabaseUserManager.getInstance(getBaseContext());
-
-        Intent intent = getIntent();
-        user = (UserPartial) intent.getSerializableExtra("USER");
+        //display user's avatar
         // The avatar, stored in user.avatar, is either null or a String representing the address of the image
-        System.out.println(user.avatar);
-
-
+        if(user.avatar == null){
+            //notify user if avatar isn't set
+            Toast.makeText(getApplicationContext(),"User has no avatar",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // need to create a new thread to load data from web
+            // https://stackoverflow.com/a/14443056
+            Thread profilepicthread = new Thread(() -> {
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(new URL(user.avatar).openConnection().getInputStream());
+                    // ui can't be updated in thread
+                    // https://stackoverflow.com/a/5162096
+                    runOnUiThread(() -> profilePicture.setImageBitmap(bitmap));
+                } catch (IOException e) {
+                    //if image can't be found
+                    Toast.makeText(getApplicationContext(),"Unable to get avatar from url",Toast.LENGTH_SHORT).show();
+                }
+            });
+            profilepicthread.start();
+        }
     }
+
+
+
 
     public View.OnClickListener search_listener = (view) -> {
         Intent intent = new Intent();
