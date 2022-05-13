6a76de59b80dbc361de2543d2df4ef4520f3909f
Zane Gates <u7334218@anu.edu.au>
2022-05-13 22:25:47 +1000

    Modified template to allow changing language preferences and display names. These do not yet have an affect on the actual user.

--- app/src/main/java/com/example/comp2100_assignment/AccountSettingsActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/AccountSettingsActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +28,16 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         int[] switchIDs = {R.id.switchMusic, R.id.switchTravel, R.id.switchFood, R.id.switchSports};
         ConversationTopic[] topics = {ConversationTopic.MUSIC, ConversationTopic.TRAVEL, ConversationTopic.FOOD, ConversationTopic.SPORTS};
+
+        int[] languageSpinnerIDs = {R.id.languageSpinner1, R.id.languageSpinner2, R.id.languageSpinner3, R.id.languageSpinner4, R.id.languageSpinner5};
+        Language[] languages = {Language.ENGLISH, Language.ITALIAN, Language.GERMAN, Language.FRENCH, Language.JAPANESE};
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languageSkillLevels, android.R.layout.simple_spinner_item);
+
+        for (int spinnerID : languageSpinnerIDs) {
+            Spinner spinner = (Spinner) findViewById(spinnerID);
+            spinner.setAdapter(adapter);
+        }
 
         switches = new Switch[switchIDs.length];
         for (int i = 0; i < switches.length; i++) {
@@ -50,7 +62,7 @@ public class AccountSettingsActivity extends AppCompatActivity {
                 }
                 DatabaseReference userReference = DatabaseUserManager.getInstance(getBaseContext()).getDatabase().getReference("userList").child(user.getUsername());
 
-                userReference.child("topics").setValue(topicString.toString());
+                userReference.child("conversationTopics").setValue(topicString.toString());
 
                 Intent intent = new Intent();
                 intent.setClass(AccountSettingsActivity.this, MainActivity.class);
