bab4b591af60690593da2eb08c57e420bdab61a6
William <u7283599@anu.edu.au>
2022-05-18 00:14:05 +1000

    Added QueueActivityTests class - all tests outside of LoginActivity won't work without user mocking somehow

---
+++ app/src/androidTest/java/com/example/comp2100_assignment/QueueActivityTests.java
@@ -0,0 +1,71 @@
+package com.example.comp2100_assignment;
+
+import static androidx.core.content.UnusedAppRestrictionsConstants.ERROR;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+//@RunWith(AndroidJUnit4.class)
+public class QueueActivityTests {
+    //sets the activity before and after every test is run
+    //this example sets to loginActivity directly
+    /*
+    @Rule
+    public ActivityScenarioRule<QueueActivity> queueActivityTestRule = new ActivityScenarioRule<QueueActivity>(QueueActivity.class);
+
+
+    //Tests that all elements are displayed
+    //@Test
+    public void allElementsDisplayed() {
+        onView(withId(R.id.textView3)).check(matches(isCompletelyDisplayed()));
+        onView(withId(R.id.exitQueueButton)).check(matches(isCompletelyDisplayed()));
+    }
+
+    //checks exit queue button exits
+    //does this by checking that title_app ("CHAT ROOM" text) is displayed after button press
+   // @Test
+    public void exitQueueButtonWorks(){
+        Intents.init();
+        onView(withId(R.id.exitQueueButton)).perform(click());
+        intended(hasComponent("MainActivity"));
+    }
+
+*/
+
+}
