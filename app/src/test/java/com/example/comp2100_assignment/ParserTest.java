41b94545ac590a63593e626309c37771eda3eef8
William <u7283599@anu.edu.au>
2022-05-19 20:22:48 +1000

    Added tests for Parser and Interaction classes

---
+++ app/src/test/java/com/example/comp2100_assignment/ParserTest.java
@@ -0,0 +1,43 @@
+package com.example.comp2100_assignment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
+public class ParserTest {
+
+
+    //testing that parser correctly rejects invalid input
+    @Test (expected = Parser.IllegalProductionException.class)
+    public void testException1(){
+        String invalid = "not not";
+        Tokenizer tokenizer = new Tokenizer(invalid);
+        Parser parser = new Parser(tokenizer);
+        parser.parseExp();
+    }
+    @Test (expected = Parser.IllegalProductionException.class)
+    public void testException2(){
+        String invalid = "ENGLISH and not and";
+        Tokenizer tokenizer = new Tokenizer(invalid);
+        Parser parser = new Parser(tokenizer);
+        parser.parseExp();
+    }
+    @Test (expected = Parser.IllegalProductionException.class)
+    public void testException3(){
+        String invalid = "FOOD or TRAVEL and ENGLISH or";
+        Tokenizer tokenizer = new Tokenizer(invalid);
+        Parser parser = new Parser(tokenizer);
+        parser.parseExp();
+    }
+    @Test (expected = Parser.IllegalProductionException.class)
+    public void testException4(){
+        String invalid = "or ENGLISH and ENGLISH";
+        Tokenizer tokenizer = new Tokenizer(invalid);
+        Parser parser = new Parser(tokenizer);
+        parser.parseExp();
+    }
+
+}
