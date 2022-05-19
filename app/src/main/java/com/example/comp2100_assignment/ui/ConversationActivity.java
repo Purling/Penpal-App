44ad5c22829cc852f0008f3dfbad213067a8e8da
u7312578 <u7312578@anu.edu.au>
2022-05-19 17:33:39 +1000

    Made sure that permanent conversations don't constantly display "joined"

--- app/src/main/java/com/example/comp2100_assignment/ui/ConversationActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/ui/ConversationActivity.java
@@ -1,30 +1,28 @@
 package com.example.comp2100_assignment.ui;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentContainerView;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.comp2100_assignment.R;
 import com.example.comp2100_assignment.conversations.ConversationFormer;
 import com.example.comp2100_assignment.conversations.ConversationTopic;
 import com.example.comp2100_assignment.conversations.TransitoryConversation;
 import com.example.comp2100_assignment.conversations.UserMessage;
 import com.example.comp2100_assignment.database.DatabaseUserManager;
 import com.example.comp2100_assignment.users.Language;
-import com.example.comp2100_assignment.R;
 import com.example.comp2100_assignment.users.User;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -37,8 +35,11 @@ import java.net.URL;
 import java.util.ArrayList;
 
 /**
+ * Visualises and allows users to interact with transitory and permanent conversations.
+ * Transitory conversations are made using the queueing system.
+ * Permanent conversations are between users who are friends.
+ *
  * @author Zane Gates
- * visualises and allows users to interact with transitory and permanent conversations
  */
 public class ConversationActivity extends AppCompatActivity {
 
@@ -64,12 +65,15 @@ public class ConversationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
+        // Get all the information passed through from intent
         Intent intent = getIntent();
         user = (User) intent.getSerializableExtra("user");
         String conversationName = intent.getStringExtra("conversationName");
         boolean permanent = intent.getBooleanExtra("permanent", false);
         String queueName = permanent ? "" : intent.getStringExtra("queueName");
         owner = intent.getBooleanExtra("owner", false);
+
+        // Get UI elements
         messageBox = findViewById(R.id.messageBox);
         messageBox.setInputType(InputType.TYPE_NULL); // Hides the keyboard
         conversation = findViewById(R.id.conversation);
@@ -84,16 +88,23 @@ public class ConversationActivity extends AppCompatActivity {
 
         conversationRoot.child(owner ? "user1" : "user2").setValue(user.getUsername());
 
-        UserMessage message = new UserMessage("", user.getUsername() + " joined.");
-        conversationMessagesRoot.child(String.valueOf(message.hashCode())).setValue(message);
+        // Only make the message joined if the conversation is not permanent
+        if (!permanent) {
+            UserMessage message = new UserMessage("", user.getUsername() + " joined.");
+            conversationMessagesRoot.child(String.valueOf(message.hashCode())).setValue(message);
+
+        }
 
         conversationMessagesRoot.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 UserMessage message = snapshot.getValue(UserMessage.class);
                 boolean added = false;
+                if (message != null && permanent && message.author.equals("")) {
+                    return;
+                }
                 for (int i = 0; i < messages.size(); i++) {
-                    if (Long.valueOf(message.time) < Long.valueOf(messages.get(i).time)) {
+                    if (Long.parseLong(message.time) < Long.parseLong(messages.get(i).time)) {
                         messages.add(i, message);
                         added = true;
                         break;
@@ -104,31 +115,32 @@ public class ConversationActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {}
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {}
-        });
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
 
-        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                UserMessage message = new UserMessage(user.getUsername(), messageBox.getText().toString());
-                conversationMessagesRoot.child(String.valueOf(message.hashCode())).setValue(message);
-                messageBox.setText("");
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
 
-        findViewById(R.id.exitConversationButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!permanent)
-                    availableReference.setValue("#CLOSED");
-                returnToMainActivity(permanent);
-            }
+        findViewById(R.id.sendButton).setOnClickListener(view -> {
+            UserMessage message = new UserMessage(user.getUsername(), messageBox.getText().toString());
+            conversationMessagesRoot.child(String.valueOf(message.hashCode())).setValue(message);
+            messageBox.setText("");
+        });
+
+        findViewById(R.id.exitConversationButton).setOnClickListener(view -> {
+            if (!permanent)
+                availableReference.setValue("#CLOSED");
+            returnToMainActivity(permanent);
         });
 
         availableReference.addValueEventListener(new ValueEventListener() {
@@ -154,7 +166,7 @@ public class ConversationActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ConversationTopic topic = snapshot.getValue(ConversationTopic.class);
-                ((TextView)findViewById(R.id.conversationTopic)).setText(topic == null ? "" : topic.toString());
+                ((TextView) findViewById(R.id.conversationTopic)).setText(topic == null ? "" : topic.toString());
             }
 
             @Override
@@ -167,7 +179,7 @@ public class ConversationActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Language language = snapshot.getValue(Language.class);
-                ((TextView)findViewById(R.id.language1)).setText(language == null ? "" : language.toString());
+                ((TextView) findViewById(R.id.language1)).setText(language == null ? "" : language.toString());
             }
 
             @Override
@@ -180,7 +192,7 @@ public class ConversationActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Language language = snapshot.getValue(Language.class);
-                ((TextView)findViewById(R.id.language2)).setText(language == null ? "" : language.toString());
+                ((TextView) findViewById(R.id.language2)).setText(language == null ? "" : language.toString());
             }
 
             @Override
@@ -198,12 +210,13 @@ public class ConversationActivity extends AppCompatActivity {
                 if (u == null) return;
                 if (userNumber == 1) user1 = u;
                 if (userNumber == 2) user2 = u;
-                ((TextView)findViewById(label)).setText(u.getDisplayName());
+                ((TextView) findViewById(label)).setText(u.getDisplayName());
                 new Thread(() -> {
                     try {
                         Bitmap bitmap = BitmapFactory.decodeStream(new URL(u.getAvatar()).openConnection().getInputStream());
-                        runOnUiThread(() -> ((ImageView)findViewById(avatarID)).setImageBitmap(bitmap));
-                    } catch (Exception ignored) {}
+                        runOnUiThread(() -> ((ImageView) findViewById(avatarID)).setImageBitmap(bitmap));
+                    } catch (Exception ignored) {
+                    }
                 }).start();
                 if (owner && !metadataGenerated && user1 != null && user2 != null) {
                     metadataGenerated = true;
@@ -232,13 +245,13 @@ public class ConversationActivity extends AppCompatActivity {
         //clear everything
         messageLayout.removeAllViewsInLayout();
         //display each message
-        for(UserMessage message : messages){
+        for (UserMessage message : messages) {
             //generate the view
             LinearLayout linearLayout = new LinearLayout(this);
             TextView messageAuthor = new TextView(this);
             messageAuthor.setText(message.author);
             messageAuthor.setTextSize(20);
-            messageAuthor.setPadding(0,0,20,0);
+            messageAuthor.setPadding(0, 0, 20, 0);
             TextView conversationText = new TextView(this);
             conversationText.setText(message.content);
             conversationText.setTextSize(16);
--- app/src/main/java/com/example/comp2100_assignment/ui/FriendActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/ui/FriendActivity.java
@@ -2,8 +2,6 @@ package com.example.comp2100_assignment.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,18 +12,21 @@ import com.example.comp2100_assignment.users.User;
 import java.util.ArrayList;
 
 /**
+ * Allows users to visit conversations with any of their friends.
+ * These conversations are persistent and stored within the database.
+ *
  * @author Zane Gates
- * allows users to visit conversations with any of their friends
  */
 public class FriendActivity extends TabbedActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
-        user = (User) getIntent().getSerializableExtra("user");
 
+        user = (User) getIntent().getSerializableExtra("user");
         tabs = new UITabs(findViewById(R.id.tabs), this);
 
+        // Gets all of the user's friends
         ArrayList<String> friends = new ArrayList<>();
         ArrayList<String> targetConversations = new ArrayList<>();
         for (String key : user.getFriends().keySet()) {
@@ -33,26 +34,24 @@ public class FriendActivity extends TabbedActivity {
             targetConversations.add(user.getFriends().get(key));
         }
 
+        // Displays all the friends in a list
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 getApplicationContext(),
                 android.R.layout.simple_list_item_1,
                 friends);
-
         ListView listView = findViewById(R.id.friend_list);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String conversationTarget = targetConversations.get(i);
-                Intent intent = new Intent();
-                intent.setClass(FriendActivity.this, ConversationActivity.class);
-                intent.putExtra("user", user);
-                intent.putExtra("conversationName", conversationTarget);
-                intent.putExtra("permanent", true);
-                intent.putExtra("owner", true);
-                startActivity(intent);
-            }
+        // Logic for what happens when a friend is clicked (the conversation should open)
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            String conversationTarget = targetConversations.get(i);
+            Intent intent = new Intent();
+            intent.setClass(FriendActivity.this, ConversationActivity.class);
+            intent.putExtra("user", user);
+            intent.putExtra("conversationName", conversationTarget);
+            intent.putExtra("permanent", true);
+            intent.putExtra("owner", true);
+            startActivity(intent);
         });
     }
 }
