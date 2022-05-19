d1fd9c41245e8c81fe2649bf43bb4e743da15922
Xingkun Chen <u6710622@anu.edu.au>
2022-05-19 15:07:50 +1000

    SearchActivity Update, But parser problem still not fixed

--- app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
@@ -27,16 +27,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+
 
 public class SearchActivity extends TabbedActivity {
     private Button button;
     private ListView listView;
     private EditText editText;
-    private List<String> arrayList = new ArrayList<String>();
     private List<String> listViewList = new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("username");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +44,7 @@ public class SearchActivity extends TabbedActivity {
 
         tabs = new UITabs(findViewById(R.id.tabs), this);
 
-        Intent intent= getIntent();
+        Intent intent = getIntent();
         intent.getStringExtra("search");
         startActivity(intent);
 
@@ -53,11 +52,15 @@ public class SearchActivity extends TabbedActivity {
         editText = findViewById(R.id.Input);
         listView = findViewById(R.id.result);
         button.setOnClickListener(find_Listener);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, listViewList);
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, listViewList);
         listView.setAdapter(arrayAdapter);
+    }
+    public View.OnClickListener find_Listener = (view) -> {
+
         String input = editText.getText().toString();
         Tokenizer tokenizer = new Tokenizer(input);
         String parser = new Parser(tokenizer).parseExp().show();
+
         String[] content = parser.split(",");
         ArrayList<String> contain = new ArrayList<String>();
         ArrayList<String> Notcontain = new ArrayList<String>();
@@ -65,9 +68,10 @@ public class SearchActivity extends TabbedActivity {
         for (int i = 0; i < content.length; i++) {
             if (content[i].contains("not")){
                 String word = content[i].substring(4);
+                word = word.trim();
                 Notcontain.add(word);
             }else
-                contain.add(content[i]);
+                contain.add(content[i].trim());
         }
 
 
@@ -77,80 +81,80 @@ public class SearchActivity extends TabbedActivity {
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
                     User user = dataSnapshot.getValue(User.class);
                     String username = user.getUsername();
-
+                    listViewList.clear();
                     for (int i = 0; i < contain.toArray().length; i++) {
                         String word = (String) contain.toArray()[i];
-                        if (!arrayList.contains(username)) {
+                        if (!listViewList.contains(username)) {
                             if (word.equalsIgnoreCase("ENGLISH") & user.getFamiliarity(Language.ENGLISH) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("ITALIAN") & user.getFamiliarity(Language.ITALIAN) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("GERMAN") & user.getFamiliarity(Language.GERMAN) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("FRENCH") & user.getFamiliarity(Language.FRENCH) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("JAPANESE") & user.getFamiliarity(Language.JAPANESE) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("KOREAN") & user.getFamiliarity(Language.KOREAN) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("MANDARIN") & user.getFamiliarity(Language.MANDARIN) != Familiarity.UNINTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("MUSIC") & user.getConversationTopic(ConversationTopic.MUSIC) == Interestedness.INTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("SPORTS") & user.getConversationTopic(ConversationTopic.SPORTS) == Interestedness.INTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("FOOD") & user.getConversationTopic(ConversationTopic.FOOD) == Interestedness.INTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                             if (word.equalsIgnoreCase("TRAVEL") & user.getConversationTopic(ConversationTopic.TRAVEL) == Interestedness.INTERESTED) {
-                                arrayList.add(word);
+                                listViewList.add(word);
                             }
                         }
                         if (Notcontain.toArray().length > 0) {
                             for (int j = 0; j < Notcontain.toArray().length; j++) {
                                 String notCont = (String) Notcontain.toArray()[j];
-                                if (arrayList.contains(username)) {
-                                    if (word.equalsIgnoreCase("ENGLISH") & user.getFamiliarity(Language.ENGLISH) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                if (listViewList.contains(username)) {
+                                    if (notCont.equalsIgnoreCase("ENGLISH") & user.getFamiliarity(Language.ENGLISH) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("ITALIAN") & user.getFamiliarity(Language.ITALIAN) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("ITALIAN") & user.getFamiliarity(Language.ITALIAN) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("GERMAN") & user.getFamiliarity(Language.GERMAN) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("GERMAN") & user.getFamiliarity(Language.GERMAN) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("FRENCH") & user.getFamiliarity(Language.FRENCH) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("FRENCH") & user.getFamiliarity(Language.FRENCH) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("JAPANESE") & user.getFamiliarity(Language.JAPANESE) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("JAPANESE") & user.getFamiliarity(Language.JAPANESE) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("KOREAN") & user.getFamiliarity(Language.KOREAN) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("KOREAN") & user.getFamiliarity(Language.KOREAN) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("MANDARIN") & user.getFamiliarity(Language.MANDARIN) == Familiarity.UNINTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("MANDARIN") & user.getFamiliarity(Language.MANDARIN) == Familiarity.UNINTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("MUSIC") & user.getConversationTopic(ConversationTopic.MUSIC) != Interestedness.INTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("MUSIC") & user.getConversationTopic(ConversationTopic.MUSIC) != Interestedness.INTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("SPORTS") & user.getConversationTopic(ConversationTopic.SPORTS) != Interestedness.INTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("SPORTS") & user.getConversationTopic(ConversationTopic.SPORTS) != Interestedness.INTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("FOOD") & user.getConversationTopic(ConversationTopic.FOOD) != Interestedness.INTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("FOOD") & user.getConversationTopic(ConversationTopic.FOOD) != Interestedness.INTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
-                                    if (word.equalsIgnoreCase("TRAVEL") & user.getConversationTopic(ConversationTopic.TRAVEL) != Interestedness.INTERESTED) {
-                                        arrayList.add(word);
+                                    if (notCont.equalsIgnoreCase("TRAVEL") & user.getConversationTopic(ConversationTopic.TRAVEL) != Interestedness.INTERESTED) {
+                                        listViewList.remove(notCont);
                                     }
                                 }
                             }
@@ -166,12 +170,8 @@ public class SearchActivity extends TabbedActivity {
             }
         };
         databaseReference.addListenerForSingleValueEvent(eventListener);
-    }
-    public View.OnClickListener find_Listener = (view) -> {
-        Intent intent = new Intent();
-        listViewList.clear();
-        listViewList = arrayList;
-        startActivity(intent);
     };
+
+
 
 }
