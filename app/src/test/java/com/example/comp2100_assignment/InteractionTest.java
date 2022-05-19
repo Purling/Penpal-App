e19778663016491f072205d3c0b7d9a36983816e
Zane Gates <u7334218@anu.edu.au>
2022-05-19 22:57:19 +1000

    Updated Interaction tests based on a modified implementation of Interaction

--- app/src/test/java/com/example/comp2100_assignment/InteractionTest.java
+++ app/src/test/java/com/example/comp2100_assignment/InteractionTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
+import android.icu.text.DateFormat;
+
 import com.example.comp2100_assignment.reports.Interaction;
 import com.example.comp2100_assignment.reports.InteractionType;
 
@@ -22,17 +24,17 @@ public class InteractionTest {
     public void testValid(){
         //test a sample
         Interaction interaction = new Interaction(LocalDateTime.ofEpochSecond(1234,0, ZoneOffset.UTC).toEpochSecond(ZoneOffset.UTC), InteractionType.TRANSITORY_CONVERSATION,"samplename");
-        String expected = LocalDateTime.ofEpochSecond(1234, 0, ZoneOffset.UTC) + ": " + "samplename" + " (" + InteractionType.TRANSITORY_CONVERSATION + ")";
+        String expected = DateFormat.getInstance().format(1234) + ": " + "samplename" + " (" + InteractionType.TRANSITORY_CONVERSATION + ")";
         assertEquals(expected,interaction.toString());
         //test another sample
         interaction = new Interaction(LocalDateTime.ofEpochSecond(999999999,0,ZoneOffset.UTC).toEpochSecond(ZoneOffset.UTC), InteractionType.PERMANENT_CONVERSATION, "cool convo");
-        expected = LocalDateTime.ofEpochSecond(999999999, 0, ZoneOffset.UTC) + ": " + "cool convo" + " (" + InteractionType.PERMANENT_CONVERSATION + ")";
+        expected = DateFormat.getInstance().format(999999999) + ": " + "cool convo" + " (" + InteractionType.PERMANENT_CONVERSATION + ")";
         assertEquals(expected,interaction.toString());
         //test setter methods
         interaction.setType(InteractionType.TRANSITORY_CONVERSATION);
         interaction.setDetail("not a cool convo");
         interaction.setTime(0);
-        expected = LocalDateTime.ofEpochSecond(0,0,ZoneOffset.UTC)+ ": " + "not a cool convo" + " (" + InteractionType.TRANSITORY_CONVERSATION + ")";
+        expected = DateFormat.getInstance().format(0) + ": " + "not a cool convo" + " (" + InteractionType.TRANSITORY_CONVERSATION + ")";
         assertEquals(expected,interaction.toString());
 
     }
