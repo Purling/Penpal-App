20d2bf159f78f1610dc76c223a23a941be858333
u7312578 <u7312578@anu.edu.au>
2022-05-19 23:10:17 +1000

    Fixed ListView error

--- app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
@@ -94,7 +94,7 @@ public class SearchActivity extends TabbedActivity {
                 arrayAdapter.notifyDataSetChanged();
             });
         } catch (Exception ignored) {
-
+            arrayAdapter.notifyDataSetChanged();
         }
     };
 
