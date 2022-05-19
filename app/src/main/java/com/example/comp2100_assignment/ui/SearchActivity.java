a759f16655739a1342a1121170de314403c7d623
u7312578 <u7312578@anu.edu.au>
2022-05-19 16:03:04 +1000

    Did some changes to the constraints in MainActivity

--- app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
+++ app/src/main/java/com/example/comp2100_assignment/ui/SearchActivity.java
@@ -1,8 +1,5 @@
 package com.example.comp2100_assignment.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,11 +7,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.example.comp2100_assignment.Exp;
 import com.example.comp2100_assignment.Parser;
 import com.example.comp2100_assignment.R;
 import com.example.comp2100_assignment.Tokenizer;
-import com.example.comp2100_assignment.conversations.Conversation;
 import com.example.comp2100_assignment.conversations.ConversationTopic;
 import com.example.comp2100_assignment.users.Familiarity;
 import com.example.comp2100_assignment.users.Interestedness;
@@ -29,32 +24,18 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/***
+ * Class which controls logic for searching within the app
+ *
+ * @author Xingkun Chen
+ */
 public class SearchActivity extends TabbedActivity {
+    ArrayAdapter<String> arrayAdapter;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("username");
     private Button button;
     private ListView listView;
     private EditText editText;
-    private List<String> listViewList = new ArrayList<>();
-    ArrayAdapter<String> arrayAdapter;
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("username");
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-        user = (User) getIntent().getSerializableExtra("user");
-
-        tabs = new UITabs(findViewById(R.id.tabs), this);
-
-        // What does this line even do?
-        getIntent().getStringExtra("search");
-
-        button = findViewById(R.id.search);
-        editText = findViewById(R.id.Input);
-        listView = findViewById(R.id.result);
-        button.setOnClickListener(find_Listener);
-        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, listViewList);
-        listView.setAdapter(arrayAdapter);
-    }
+    private final List<String> listViewList = new ArrayList<>();
     public View.OnClickListener find_Listener = (view) -> {
 
         String input = editText.getText().toString();
@@ -65,20 +46,20 @@ public class SearchActivity extends TabbedActivity {
         ArrayList<String> contain = new ArrayList<>();
         ArrayList<String> Notcontain = new ArrayList<>();
 
-        for (int i = 0; i < content.length; i++) {
-            if (content[i].contains("not")){
-                String word = content[i].substring(4);
+        for (String s : content) {
+            if (s.contains("not")) {
+                String word = s.substring(4);
                 word = word.trim();
                 Notcontain.add(word);
-            }else
-                contain.add(content[i].trim());
+            } else
+                contain.add(s.trim());
         }
 
 
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     User user = dataSnapshot.getValue(User.class);
                     String username = user.getUsername();
                     listViewList.clear();
@@ -172,6 +153,21 @@ public class SearchActivity extends TabbedActivity {
         databaseReference.addListenerForSingleValueEvent(eventListener);
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        user = (User) getIntent().getSerializableExtra("user");
+
+        tabs = new UITabs(findViewById(R.id.tabs), this);
+
+        button = findViewById(R.id.search);
+        editText = findViewById(R.id.Input);
+        listView = findViewById(R.id.result);
+        button.setOnClickListener(find_Listener);
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, listViewList);
+        listView.setAdapter(arrayAdapter);
+    }
 
 
 }
